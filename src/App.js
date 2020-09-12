fbc4de91d3c8403bc9108918ecd242b051943203
Shubham Gupta <gupta.shubham6262@gmail.com>
2020-09-12 13:22:56 +0530

    added collection preview on shop page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./pages/homepage/homepage.component";
+import Homepage from "./pages/homepage";
 import { Route } from "react-router-dom";
-
-const Hats = () => <div>Hats</div>;
+import ShopPage from "./pages/shop";
 
 const App = () => {
   return (
     <div>
       <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop/hats" component={Hats} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 };
