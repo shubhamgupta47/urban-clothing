d16724aa4d773ba353425100f4c235c4bffe2f8d
Shubham Gupta <gupta.shubham6262@gmail.com>
2020-09-20 14:32:12 +0530

    toggle cart feature

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./cart-dropdown.styles.scss";
+import Button from "../button/button.component";
+
+const CartDropdown = () => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items"></div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
+
+export default CartDropdown;
