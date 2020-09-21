64a70106164de25158099a46eef441d9f54209ce
Shubham Gupta <gupta.shubham6262@gmail.com>
2020-09-21 14:49:26 +0530

    added redux-persist to cache state in localStorage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,30 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //Local storage
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+/**
+ * For redux-persist
+ */
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"], //reducer name in string which we want to persist
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
+
+/**
+ * Not exporting anymore since we are using redux-persist
+ */
+// export default combineReducers({
+//   user: userReducer,
+//   cart: cartReducer,
+// });
