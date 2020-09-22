d29fe020cda45d3db9eb063ff343c05c73f4da1c
Shubham Gupta <gupta.shubham6262@gmail.com>
2020-09-22 09:39:17 +0530

    converted shop data to hash-tables

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 const selectCollections = createSelector(
@@ -15,11 +7,15 @@ const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollectionCategory = (collectionUrlParam) => {
-  return createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  return createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
 };
 
