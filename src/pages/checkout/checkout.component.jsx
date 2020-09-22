648da6b5ffb63402dde15b3de034bc2f4472502c
Shubham Gupta <gupta.shubham6262@gmail.com>
2020-09-22 12:03:09 +0530

    added stripe payment and favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,7 @@ import {
 } from "../../redux/cart/cart.selectors";
 import "./checkout.styles.scss";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, totalPrice }) => {
   return (
@@ -29,14 +30,13 @@ const CheckoutPage = ({ cartItems, totalPrice }) => {
           <span>Remove</span>
         </div>
       </div>
-
       {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
-
       <div className="total">
         <span>TOTAL: ${totalPrice}</span>
       </div>
+      <StripeCheckoutButton price={totalPrice} />
     </div>
   );
 };
