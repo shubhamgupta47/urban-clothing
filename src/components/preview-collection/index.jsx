d216a7e2c16adf75e7a8e133709b60a16bb0bd34
Shubham Gupta <gupta.shubham6262@gmail.com>
2020-09-24 13:20:41 +0530

    added WithSpinner HOC

--- src/components/preview-collection/index.jsx
+++ src/components/preview-collection/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./preview-collection.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 
-const PreviewCollection = ({ title, items }) => {
+const PreviewCollection = ({ title, items, routeName }) => {
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <Link to={`/shop/${routeName}`}>
+        <h1 className="title">{title.toUpperCase()}</h1>
+      </Link>
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -2,36 +2,79 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import {
   firestore,
   convertCollectionSnapshotToMap,
 } from "../../firebase/firebase.utils";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 import "./shop.scss";
 import { updateCollections } from "../../redux/shop/shop.actions";
+import WithSpinner from "../../hoc/with-spinner/with-spinner.component";
+
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 
 class ShopPage extends Component {
+  state = {
+    loading: true,
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    collectionRef.onSnapshot(async (snapshot) => {
+    /**
+     * This is REST API call
+     */
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/urbn-clothing-db/databases/(default)/documents/collections"
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log("kk", collections));
+
+    /**
+     * This is Promise based. The method .get() makes
+     * API call to fetch data associated with 'collectionRef'.
+     */
+
+    collectionRef.get().then((snapshot) => {
       const collectionMap = convertCollectionSnapshotToMap(snapshot);
       updateCollections(collectionMap);
+      this.setState({ loading: false });
     });
+
+    /**
+     * This is observable pattern, which supports live reloading
+     */
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+    //   async (snapshot) => {
+    //     const collectionMap = convertCollectionSnapshotToMap(snapshot);
+    //     updateCollections(collectionMap);
+    //     this.setState({ loading: false });
+    //   }
+    // );
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
